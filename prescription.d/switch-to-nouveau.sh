f56337d5b0700a0f071072462dd3f65b3e79f73d
Владислав Бабенко <65094576+vlad196@users.noreply.github.com>
2024-04-02 17:00:49 +0200

    Обновил switch-to-nouveau.sh
    - Убрал метод загрузки драйверов через -X
    -  Добавил скрипты проверки ядра из switch-to-nvidia
    - Добавил установку пакетов
    - Добавил скрипты по удалению файлов конфигурации nvidia
    - Добавил скрипт для проверки и удаления фикса "неизвестный монитор"

--- prescription.d/switch-to-nouveau.sh
+++ prescription.d/switch-to-nouveau.sh
@@ -11,41 +11,65 @@ exit
 
 # https://www.altlinux.org/Nvidia#Смена_открытых_драйверов_на_проприетарные[1]
 
-epm update || exit
-epm update-kernel || exit
-# epm full-upgrade || exit
+epm update || fatal
+epm update-kernel || fatal
 
-# TODO: проверить, совпадает ли ядро
-# reboot now
+# проверяем, совпадает ли ядро (пока нет такой проверки в update-kernel)
+USED_KFLAVOUR="$(uname -r | awk -F'-' '{print $(NF-2)}')-def"
+check_run_kernel () {
+    # TODO: support kernel-image-rt
+    ls /boot | grep "vmlinuz" | grep -vE 'vmlinuz-un-def|vmlinuz-std-def' | grep "${USED_KFLAVOUR}" | sort -Vr | head -n1 | grep -q $(uname -r)
+}
 
-# TODO
-#kernel-modules-drm-nouveau-std-def   (un-def)
-#xorg-drv-nouveau
-#xorg-dri-nouveau
+#TODO: добавить удаление драйверов nvidia. Возможно через скрипт
 
-echo "Set nouveau in /etc/X11/xorg.conf.d/10-monitor.conf"
-a= xsetup-monitor -d nouveau
+if check_run_kernel ; then
+	echo "Запущено самое свежее установленное ${USED_KFLAVOUR} ядро."
+else
+	echo "В системе есть ${USED_KFLAVOUR} ядро свежее запущенного."
+	echo "Перезагрузитесь со свежим ${USED_KFLAVOUR} ядром и перезапустите: epm play switch-to-nvidia"
+	fatal
+fi
 
-# TODO
-grep nvidia /etc/X11/xorg.conf.d/*.conf /etc/X11/xorg.conf
+Устанавливаем kernel-modules-drm-nouveau-std-def(un-def) xorg-drv-nouveau и xorg-dri-nouveau
+epm install --skip-installed xorg-drv-nouveau xorg-dri-nouveau-${USED_KFLAVOUR}  || fatal
 
-#/etc/modprobe.d/blacklist-nvidia-x11.conf и записываем туда:[1]
-#blacklist nvidia
-#blacklist nouveau
+# Проверяем, существует ли файлы файлы конфигурации, установленные для nvidia драйверов
+/etc/modprobe.d/blacklist-nvidia-x11.conf и /etc/modprobe.d/blacklist-alterator-x11.conf. Если есть, то удаляем.
+if [ -f "/etc/modprobe.d/blacklist-nvidia-x11.conf" ]; then
+  rm -f /etc/modprobe.d/blacklist-nvidia-x11.conf
+fi
+if [ -f "/etc/modprobe.d/blacklist-alterator-x11" ]; then
+  rm -f /etc/modprobe.dblacklist-alterator-x11.conf
+fi
+if [ -f "/etc/modprobe.d/nvidia_videomemory_allocation.conf" ]; then
+  rm -f /etc/modprobe.d/nvidia_videomemory_allocation.conf
+fi
+# Проверяем, все файлы конфигурации, и удаление строк с "blacklist nouveau" и "options nvidia"
+for filename in "/etc/modprobe.d/*; do
+    if [ -f "$filename" ]; then
+        # Проверяем, есть ли строки "blacklist nouveau" или "options nvidia" в файле
+        if grep -qE "blacklist nouveau|options nvidia" "$filename"; then
+            # Удаляем строки из файла
+            sed -i '/blacklist nouveau\|options nvidia/d' "$filename"
+        fi
+    fi
+done
 
-# TODO
-# rm -f /etc/modprobe.d/blacklist-alterator-x11
-# или наоборот записываем в него вместо блокировки nouveau , блокировку nvidia
+# Удаляем фикс "неизвестный монитор", если он существует
+if grep "initcall_blacklist=simpledrm_platform_driver_init" /etc/sysconfig/grub2 &>/dev/null ; then 
+	echo "Создание копии /etc/sysconfig/grub2..."
+	cp /etc/sysconfig/grub2 /etc/sysconfig/grub2.epmbak
+  sed -i 's/initcall_blacklist=simpledrm_platform_driver_init//' /etc/sysconfig/grub2
+fi
 
-# TODO
-a= x11presetdrv
-a= ldconfig
+a= x11presetdrv # сканирует PCI в /sys на предмет видеоплат производителя NVIDIA. Если таковые найдены, ищет пары драйверов ядерный+X-овый, совпадающие по версии. Переключает /lib/modules/`uname -r`/nVidia/nvidia.ko на выбранную версию
+a= ldconfig # обновляет кэш информации о новейших версиях разделяемых библиотек
 
-# И не обязательно перезагружаться?
-a= make-initrd -k $(uname -r)
+echo "Запускаем регенерацию initrd."
+make-initrd
 
-# /usr/bin/nvidia-clean-driver
+echo "Обновляем grub..."
+update-grub
 
-# TODO: https://www.altlinux.org/Nvidia#Замена_драйверов_nouveau/nvidia_"на_лету"
-
-echo "Done. Just you need reboot your system to use open source nouveau drivers for NVIDIA cards."
+echo "Выполнено. Перезагрузите систему для использования проприетарных драйверов nvidia."
